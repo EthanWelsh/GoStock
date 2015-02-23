dd357f512980094a8a34f13dc2a3f141d42dc7f1
EthanWelsh <welshej@gmail.com>
2015-02-22 20:14:56 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+        "code.google.com/p/plotinum/plot"
+        "code.google.com/p/plotinum/plotter"
+        "code.google.com/p/plotinum/plotutil"
+)
+
+func main() {
+	
+
+
+}
